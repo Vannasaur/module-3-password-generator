78ce3f98714ed392744043b0d3c35d5c59a67fa7
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-08-24 16:26:47 -0700

    sixth commit, updated JavaScript code and added content to README.md, safe revert point

--- Develop/script.js
+++ Develop/script.js
@@ -4,43 +4,38 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
-function writePassword() {
-  // const password = generatePassword (); // still need to define this!???????????????????????????????
+function generatePassword() {
   const passwordLength = prompt("Enter desired password length:");
   const convertInt = parseInt(passwordLength);
-  const passwordText = document.querySelector("#password");
 
-// Check if length entered is a valid number
+  // Check if length entered is a valid number
 
-if (isNaN(convertInt)) {
-  alert("Invalid input. Please enter a valid number.");
-  return;
-}
+  if (isNaN(convertInt)) {
+    alert("Invalid input. Please enter a valid number.");
+    return "";
+  }
 
-// Check password length
+  // Check password length
 
-if (convertInt < 8 || convertInt > 128) {
-  alert("Password length must be between 8 and 128.");
-  return;
-}
+  if (convertInt < 8 || convertInt > 128) {
+    alert("Password length must be between 8 and 128.");
+    return "";
+  }
+  // Define options for password generation
 
-// Define options for password generation
-
-  const lowerAbc = "qwertyuiopasdfghjklzxcvbnm".split("").sort();
+  const lowerAbc = "qwertyuiopasdfghjklzxcvbnm".split("").sort(); 
   const upperAbc = "QWERTYUIOPASDFGHJKLZXCVBNM".split("").sort();
   const num = "1234567890".split("").sort();
   const specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\".split("");
-  
+
   const lowerAbcPass = confirm("Would you like to include lowercase letters?");
   const upperAbcPass = confirm("Would you like to include uppercase letters?");
   const numPass = confirm("Would you like to include numbers?");
   const specialCharPass = confirm("Would you like to include special characters?");
 
-//If confirmed, the password criteria will be added to the character set array
+  // If confirmed, the password criteria will be added to the character set array
 
-let charSet = [];
+  let charSet = [];
 
   if (lowerAbcPass) {
     charSet = charSet.concat(lowerAbc);
@@ -58,35 +53,26 @@ let charSet = [];
     charSet = charSet.concat(specialChar);
   }
 
+  // Use character set array to randomize password
+
   let password = "";
 
-  for (let i=0; i < convertInt; i++) {
+  for (let i = 0; i < convertInt; i++) {
     const randomIndex = Math.floor(Math.random() * charSet.length);
     password = password + charSet[randomIndex];
   }
+  return password;
+}
+
+// Write password to the #password input
+
+function writePassword() {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-// Testing Area - Please disregard //
-// const lowerAbc = "qwertyuiopasdfghjklzxcvbnm".split("").sort();
-// console.log(lowerAbc);
-// const upperAbc = "QWERTYUIOPASDFGHJKLZXCVBNM".split("").sort();
-// console.log(upperAbc);
-// const num = "1234567890".split("").sort();
-// console.log(num);
-// const specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\".split("").sort();
-// console.log(specialChar);
-// const specialChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\".split("").sort(); 
-// console.log(specialChar);
-// const doubleQuotes = "\"";
-// console.log(doubleQuotes);
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Password Generator Starter Code
+# Password Generator - Module 3 Challenge
+
+## Description
+
+This Password Generator Webpage was created for boot camp students practicing JavaScript. The starter code was provided and the students were instructed to make the webpage functional. In order to generate a secure password, the students needed to ask the user to provide the desired character length and confirm which criteria the user wanted to include in the password. Please see User Story and Acceptance Criteria below: 
+
+User Story: 
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+Acceptance Criteria:
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Installation
+
+N/A
+
+## Usage
+
+To use the Password Generator, click on the Generate Password button.
+
+ A prompt will appear, which is where you will enter your desired password length. If the amount entered is not a number, an error alert will pop-up. If the amount entered is a number but not within the criteria range of 8-128, an alert will notify you of the acceptance criteria range. 
+
+ Once you have entered a number between 8-128, you will be asked additional questions regarding the types of characters you would like to include in your password. If you select at least one of the character options, your password will be generated and displayed in the Secure Password textbox. 
+
+ Link to deployed application: https://vannasaur.github.io/module-3-password-generator/
+
+## Credits
+
+Password Generator Starter Code from github user: Xandromus in Repository: friendly-parakeet. See below for link to repository: https://github.com/coding-boot-camp/friendly-parakeet
+
+## License
+
+MIT License, please refer to the LICENSE in the repo.
+
+
+![_Users_giovannaluciano_bootcamp_challenges_module-3_Develop_index html](https://github.com/Vannasaur/module-3-password-generator/assets/141793843/f50258e3-a0bc-40af-8eda-09da003b2936)
